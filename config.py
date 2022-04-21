3631f949c72d15f47c81026de8eff8d9accfaabd
#Ge͂̂͝c̨͒ͤ̕͜e̡͝ k̶̴̛ͮ͌̒̃͟͡uş̟̲͎u̼͓̭̝ͦ̂͋̈̋̔ <96833319+Gecekus@users.noreply.github.com>
2022-04-21 12:32:02 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,10 +7,14 @@ admins = {}
 
 SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
-BOT_NAME = getenv("BOT_NAME", "TaliaMüzik") 
+BOT_NAME = getenv("BOT_NAME")
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
-BOT_USERNAME = getenv("BOT_USERNAME", "Efsanestar_bot")
+BOT_USERNAME = getenv("BOT_USERNAME") 
+ASSISTANT_NAME = getenv("ASSISTANT_NAME")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL") 
+OWNER_NAME = getenv("OWNER_NAME") 
 PMPERMIT = getenv("PMPERMIT", "ENABLE")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
 
