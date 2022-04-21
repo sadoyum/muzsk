77c8a0df552a22930cae1cbff6b5bb727d4f13de
#Ge͂̂͝c̨͒ͤ̕͜e̡͝ k̶̴̛ͮ͌̒̃͟͡uş̟̲͎u̼͓̭̝ͦ̂͋̈̋̔ <96833319+Gecekus@users.noreply.github.com>
2022-04-21 12:25:58 +0300

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -109,7 +109,7 @@ async def play(_, message: Message):
     try:
         user = await USER.get_me()
     except:
-        user.first_name = "EfsaneMusicVaves"
+        user.first_name = "Senin_Sarkın"
     usar = user
     wew = usar.id
     try:
